dcd25ad9ce0a6ea844a2ccd08be820a735efbcd3
nsmrtzv <samartsevnikita@gmail.com>
2022-11-22 01:15:17 +0300

    change project structure; add .idea to .gitignore

--- project/webserver.tf
+++ project/webserver.tf
@@ -1,6 +1,6 @@
 module "webserver_instance" {
   # count = ?
-  source           = "./instance_module"
+  source           = "../modules/aws/aws_instance"
   instance_profile = aws_iam_instance_profile.web_console_session_profile.id
   security_group   = aws_security_group.webserver_sg.id
 }
